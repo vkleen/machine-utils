166a316e252e4c5ae63ca10bdd7cb4170b42eb3c
Viktor Kleen <viktor@kleen.org>
2022-03-21 20:51:59 +0000

    Make macname input explicit

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+  inputs = {
+    macname.url = github:vkleen/macname;
+  };
   outputs = { self, nixpkgs, macname, ... }@inputs: {
     lib = import ./. { inherit (inputs.nixpkgs) lib; inherit macname; };
   };
